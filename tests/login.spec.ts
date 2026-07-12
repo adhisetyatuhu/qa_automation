b4f1134ae1aaa20fa5e8c1e9be48e38609e79744
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2026-07-12 11:18:32 +0700

    refactor login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('login unsuccessful @negative', async ({ page }) => {
+  // precondition
   await page.goto('https://www.emra.chat/login');
+
+  // steps
   await page.getByRole('textbox', { name: 'Email' }).click();
   await page.getByRole('textbox', { name: 'Email' }).fill('test@gmail.com');
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('test');
   await page.getByRole('button', { name: 'Sign In' }).click();
+
+  // expectation
   await expect(page.getByText('Invalid credentials')).toBeVisible();
   await expect(page.getByText('Welcome Back')).toBeVisible();
 });
